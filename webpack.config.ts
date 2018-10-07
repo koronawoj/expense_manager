c10499c05a0c8eb543f7e2def699e4f79bcef543
Wojtek <koronawoj@gmail.com>
2018-10-07 12:32:55 +0200

    cleaned up webpack conf and tslint

--- webpack.config.ts
+++ webpack.config.ts
@@ -35,21 +35,5 @@ module.exports = {
   devServer: {
     port: 3000,
     open: true,
-    stats: {
-      colors: true,
-      hash: false,
-      version: false,
-      timings: false,
-      assets: false,
-      chunks: false,
-      modules: false,
-      reasons: false,
-      children: false,
-      source: false,
-      errors: false,
-      errorDetails: false,
-      warnings: false,
-      publicPath: false,
-    },
   },
 }
